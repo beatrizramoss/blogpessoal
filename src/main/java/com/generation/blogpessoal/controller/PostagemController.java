c3792a6238a7228564895b9d949138ce162212f5
Beatriz <beatriizramoss@gmail.com>
2022-07-05 09:54:54 -0300

    Atividade 03 CRUD Blog Pessoal

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -15,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
 
+@JsonIgnoreProperties
 @RestController
 @RequestMapping("/postagens")
 @CrossOrigin("*")
